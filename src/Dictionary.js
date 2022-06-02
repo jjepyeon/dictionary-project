dff4ef6cac4f97f5f235dcbec468d9193154b5c4
jjepyeon <66534630+jjepyeon@users.noreply.github.com>
2022-06-01 20:35:58 -0700

    Components for meaning and results created

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,21 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function search(event) {
     event.preventDefault();
   }
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-
   axios.get(apiUrl).then(handleResponse);
 
   function handleKeywordChange(event) {
@@ -36,6 +37,7 @@ export default function Dictionary() {
           className="searchButton"
         />
       </form>
+      <Results results={results} />
     </div>
   );
 }
