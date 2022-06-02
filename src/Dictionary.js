3f8b9ad04ee1842e34bee771d7d574629110c848
jjepyeon <66534630+jjepyeon@users.noreply.github.com>
2022-06-01 19:55:09 -0700

    axios and api integrated

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
@@ -6,8 +7,15 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
   }
+
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
+  let apiUrl = `http://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+
+  axios.get(apiUrl).then(handleResponse);
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
